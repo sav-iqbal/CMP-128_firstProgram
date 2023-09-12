5c13acc477411c08cfb47ff783ab6128c8c6a790
Savaas Iqbal <122247798+sav-iqbal@users.noreply.github.com>
2023-09-12 14:42:17 -0400

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,16 @@
+/* my first Java program for CMP-128 by Savaas Iqbal 
+created on 9/20/2022 */ 
+
+public class MyFirstProgram  {
+   public static void main (String [] args) {
+      
+      System.out.println("Hi how are you?");
+      System.out.println("My name is Savaas."); 
+      
+   }
+}
+
+/* 
+Hi how are you?
+My name is Savaas
+*/
